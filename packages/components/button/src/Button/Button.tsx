50705da9627432335d54f15bd9a5103ecb2195ba
Samuel Billaud <samuel.billaud1990@gmail.com>
2023-03-09 14:17:24 +0100

    feat: add color optional props on Button

--- packages/components/button/src/Button/Button.tsx
+++ packages/components/button/src/Button/Button.tsx
@@ -3,8 +3,9 @@ import { FC } from "react";
 type ButtonProps = {
   text: string;
   onClick: () => void;
+  color?: 'blue' | 'red';
 }
 
-export const Button: FC<ButtonProps> = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
+export const Button: FC<ButtonProps> = ({ text, onClick, color = 'blue' }) => {
+  return <button onClick={onClick} style={{ color }}>{text}</button>;
 };
