21b091ab3d49179663cf24fe26a254ac0793c84a
Samuel Billaud <samuel.billaud1990@gmail.com>
2024-01-30 21:41:59 +0100

    feat(panda-preset): add animations, blurs, borders, durations, easings, sizes & z-index & add keyframes & text-styles

--- packages/panda-preset/src/index.ts
+++ packages/panda-preset/src/index.ts
@@ -1,19 +1,23 @@
 import type { Preset } from '@pandacss/types'
 
+import { breakpoints } from './theme/breakpoints'
 import { conditions } from './conditions'
 import { globalCss } from './global-css'
-import { tokens } from './theme/tokens'
-import { breakpoints } from './theme/breakpoints'
+import { keyframes } from './theme/keyframes'
 import { semanticTokens } from './theme/semantic-tokens'
+import { textStyles } from './theme/text-styles'
+import { tokens } from './theme/tokens'
 
 const definePreset = <T extends Preset>(preset: T) => preset
 
 export const preset = definePreset({
   theme: {
     extend: {
-      tokens,
       breakpoints,
+      keyframes,
       semanticTokens,
+      textStyles,
+      tokens,
     },
   },
   conditions,
--- packages/panda-preset/src/theme/tokens/index.ts
+++ packages/panda-preset/src/theme/tokens/index.ts
@@ -1,19 +1,33 @@
 import type { Tokens } from '@pandacss/types'
 
+import { animations } from './animations'
+import { blurs } from './blurs'
+import { borders } from './borders'
+import { defineColorTokens } from './colors'
+import { durations } from './durations'
+import { easings } from './easings'
+import { radii } from './radii'
+import { sizes } from './sizes'
 import { spacing } from './spacing'
 import { fontSizes, fontWeights, fonts, letterSpacings, lineHeights } from './typography'
-import { defineColorTokens } from './colors'
-import { radii } from './radii'
+import { zIndex } from './z-index'
 
 const defineTokens = <T extends Tokens>(v: T) => v
 
 export const tokens = defineTokens({
+  animations,
+  blurs,
+  borders,
   colors: defineColorTokens(),
+  durations,
+  easings,
   fonts,
   fontSizes,
   fontWeights,
   letterSpacings,
   lineHeights,
   radii,
+  sizes,
   spacing,
+  zIndex
 })
