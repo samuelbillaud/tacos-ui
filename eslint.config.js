91c9cf5ce919ef0fc99f55750a5a4128903ebdfd
Samuel Billaud <samuel.billaud1990@gmail.com>
2024-04-06 16:58:32 +0200

    chore: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,110 +1,97 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import stylisticPlugin from '@stylistic/eslint-plugin';
-import tsEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import pluginJs from '@eslint/js';
 import prettierPlugin from 'eslint-config-prettier';
 import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural';
-import reactPlugin from 'eslint-plugin-react';
+import eslintPluginReactConfigRecommended from 'eslint-plugin-react/configs/recommended.js';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import globals from 'globals';
+import path from 'path';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
 
-const compat = new FlatCompat();
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
-const thirdPartyRules = {
-  ...tsEslint.configs.recommended.rules,
-};
-
-const config = {
-  languageOptions: {
-    parser: tsParser,
-    parserOptions: {
-      project: [
-        './tsconfig.json',
-        './packages/components/tsconfig.json',
-        './packages/panda-preset/tsconfig.json',
-      ],
-    },
-    globals: globals.browser,
-  },
-  plugins: {
-    '@typescript-eslint': tsEslint,
-    'eslint-plugin-react': reactPlugin,
-    'eslint-plugin-react-hooks': reactHooksPlugin,
-    '@stylistic': stylisticPlugin,
-  },
-  rules: {
-    ...thirdPartyRules,
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let'], next: ['const', 'let'] },
-    ],
-    'perfectionist/sort-objects': 'off',
-    'perfectionist/sort-imports': [
-      'error',
-      {
-        type: 'natural',
-        order: 'asc',
-        groups: [
-          'type',
-          'react',
-          'panda',
-          ['builtin', 'external'],
-          'styledSystem',
-          'internal-type',
-          'internal',
-          ['parent-type', 'sibling-type', 'index-type'],
-          ['parent', 'sibling', 'index'],
-          'side-effect',
-          'style',
-          'object',
-          'unknown',
-        ],
-        'custom-groups': {
-          value: {
-            react: ['react', 'react-*', 'react-*/**'],
-            panda: '@pandacss/**',
-            styledSystem: '@tacos-ui/styled-system/**',
-          },
-        },
-        'newlines-between': 'always',
-      },
-    ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-};
-
-const globalIgnores = {
-  ignores: [
-    '**/postcss.config.cjs',
-    '**/.storybook/**',
-    '**/node_modules',
-    '**/dist',
-    '**/build',
-    '**/styled-system',
-    '*.d.ts',
-  ],
-};
-
-const globalFiles = {
-  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-};
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  ...compat.config(reactPlugin.configs.recommended),
-  ...compat.config(reactPlugin.configs['jsx-runtime']),
-  ...compat.config(reactHooksPlugin.configs.recommended),
-  js.configs.recommended,
+  {
+    ignores: [
+      '**/postcss.config.cjs',
+      '**/.storybook/**',
+      '**/node_modules',
+      '**/dist',
+      '**/build',
+      '**/styled-system',
+      '*.d.ts',
+    ],
+  },
   perfectionistNatural,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', next: 'return', prev: '*' },
+        { blankLine: 'always', next: '*', prev: ['const', 'let'] },
+        { blankLine: 'any', next: ['const', 'let'], prev: ['const', 'let'] },
+      ],
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          'custom-groups': {
+            value: {
+              panda: '@pandacss/**',
+              react: ['react', 'react-*', 'react-*/**'],
+              styledSystem: '@tacos-ui/styled-system/**',
+            },
+          },
+          groups: [
+            'type',
+            'react',
+            'panda',
+            ['builtin', 'external'],
+            'styledSystem',
+            'internal-type',
+            'internal',
+            ['parent-type', 'sibling-type', 'index-type'],
+            ['parent', 'sibling', 'index'],
+            'side-effect',
+            'style',
+            'object',
+            'unknown',
+          ],
+          'newlines-between': 'always',
+          order: 'asc',
+          type: 'natural',
+        },
+      ],
+      'perfectionist/sort-objects': 'off',
+    },
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['packages/components/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      eslintPluginReactConfigRecommended,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  ...compat.config(reactHooksPlugin.configs.recommended),
   prettierPlugin,
-  config,
-  globalIgnores,
-  globalFiles,
 ];
